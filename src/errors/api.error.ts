ebea2a86e9af8f773f17469270c16fdfc2134b53
OksanaNevolina <onevolina5gmail.com>
2024-12-11 23:11:47 +0200

    change api.error

--- src/errors/api.error.ts
+++ src/errors/api.error.ts
@@ -1,7 +1,10 @@
 export class ApiError extends Error {
     public status: number;
+
     constructor(message: string, status: number) {
         super(message);
         this.status = status;
+
+        Object.setPrototypeOf(this, new.target.prototype);
     }
 }
