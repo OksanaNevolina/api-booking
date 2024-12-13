1075a842396a85172b311f5ac15baa173723f1f7
OksanaNevolina <onevolina5gmail.com>
2024-12-13 14:18:42 +0200

    added swagger

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,19 @@ import express, {
   Response,
 } from "express";
 import * as mongoose from "mongoose";
+import * as swaggerUi from "swagger-ui-express";
 
 import { configs } from "./configs/configs";
 import { ApiError } from "./errors/api.error";
 import { authRouter } from "./routers/auth.router";
 import { userRouter } from "./routers/user.router";
 import { bookingRouter } from "./routers/bookingRouter";
+import yaml from "yamljs";
+import  path from "path";
+
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+const swaggerDocument = yaml.load(path.join(__dirname, 'swagger.yaml'));
 
 const app = express();
 
@@ -22,6 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
 app.use("/bookings", bookingRouter);
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const errorHandler: ErrorRequestHandler = (
   err: ApiError,
